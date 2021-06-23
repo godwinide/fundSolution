51d95014d3ff0897529183067850aa5d45510a74
Godwin Idemudia <godwinide@gmail.com>
2021-06-23 23:53:22 +0100

    second commit

--- urls.js
+++ urls.js
@@ -1,13 +1,13 @@
 const path = require('path');
 
 module.exports = app => {
-    app.use("/api/customers", require("./routes/api/customers"));
-    app.use("/api/deposit", require("./routes/api/deposit"));
-    app.use("/api/withdraw", require("./routes/api/withdraw"));
-    app.use("/api/history", require("./routes/api/history"));
-    app.use("/api/commission", require("./routes/api/commission"));
+    app.use("/customers", require("./routes/api/customers"));
+    app.use("/deposit", require("./routes/api/deposit"));
+    app.use("/withdraw", require("./routes/api/withdraw"));
+    app.use("/history", require("./routes/api/history"));
+    app.use("/commission", require("./routes/api/commission"));
 
-    app.use("/api/admin", require("./routes/api/admin"));
+    app.use("/admin", require("./routes/api/admin"));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
